3aec9b58bb7492ccc39def65fdd60c2bba300911
bishtpawan <pawan.bisht@knoldus.in>
2019-08-19 15:41:05 +0530

    Update doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-/// Trigger the RaspberryPi Camera and capture picture.
+/// Trigger the RaspberryPi Camera to capture image.
 ///
 /// This module uses the bash command to access the Raspberry Pi's camera.
-
+///
 pub mod image {
     pub mod camera_operations;
     pub mod settings;
